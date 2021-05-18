646efece8181ef049e27ad7d7f95b800b6a66e25
Alejandro Cruz <alex_crz97@hotmail.com>
2021-05-18 04:02:18 -0500

    Seccion 4

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// FC
+const PrimeraAPP = ( { saludo, subtitulo } ) => {
+
+  return (
+    <>
+      <h1>{ saludo }</h1>
+      {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
+      <p>{ subtitulo }</p>
+    </>
+  );
+};
+
+PrimeraAPP.propTypes = {
+  saludo: PropTypes.string.isRequired
+}
+
+PrimeraAPP.defaultProps = {
+  subtitulo: ""
+}
+
+export default PrimeraAPP;
