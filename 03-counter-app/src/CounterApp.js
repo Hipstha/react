54a28b95b27bb81ae7d70238867c6e54f143e593
Alejandro Cruz <alex_crz97@hotmail.com>
2021-07-27 19:48:53 -0500

    Seccion 6

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,  PropTypes } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
---
+++ 04-gif-expert-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// components
+import GifExpertApp from './GifExpertApp/GifExpertApp';
+
+import './index.css';
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
+
+// componente llamado GifExpertApp
+// solo H" que diga Gif ExpertApp
+// y un HR
