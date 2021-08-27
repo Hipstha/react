0d63014fe148fce9d2ec399f6cd2b4ecf7f532c0
Alejandro Cruz <alex_crz97@hotmail.com>
2021-08-26 20:33:24 -0500

    Final del curso

---
+++ 07-calendar-app/src/types/types.js
@@ -0,0 +1,14 @@
+
+
+
+export const types = {
+  uiOpenModal: '[ui] Open Modal',
+  uiCloseModal: '[ui] Close Modal',
+
+  eventSetActive: '[event] Set Active',
+  eventAddNew: '[event] Add new',
+  eventClearActiveEvent: '[event] Clear active event',
+  eventUpdate: '[event] Event updated',
+  eventDeleted: '[event] Event deleted'
+  
+};
